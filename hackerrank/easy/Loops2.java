2944f589cf8f85f7d24916151550c870b962cfe7
yunajoe0620 <yunajoe@fineteacher.com>
2025-07-25 08:59:32 +0900

    Loops2.java

---
+++ hackerrank/easy/Loops2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();  
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int answer = 0;     
+            for(int j = 0; j< n; j++){
+                   
+                for(int p = 0; p<=j; p++){                   
+                    int result = (int) Math.pow(2, p);
+                    int result2 = result * b;
+                    answer += result2;
+                                                    
+                }
+            answer += a;
+            System.out.printf("%d ", answer);       
+            answer =0;
+        }           
+        System.out.println();
+    }
+    
+      in.close();
+   }    
+};
