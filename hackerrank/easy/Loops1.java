7a18ae12473d483559129c6be61807abc8ac0a2a
yunajoe <yunajoe@yunajoeui-MacBookPro.local>
2025-07-26 10:12:35 +0900

    DataType.java

--- hackerrank/easy/Loops1.java
+++ hackerrank/easy/Loops1.java
@@ -1,11 +1,22 @@
-public class Solution {
-    public static void main(String[] args) throws IOException 
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(bufferedReader.readLine().trim())
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-    for(int i=1; i<=10; i++){
-        System.out.printf("%d x %d = %d%n", N, i, N *i);  
+
+
+
+
+public class Loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
     }
-
-    bufferedReader.close();
 }
+
