c030332697de4a5021673b94e89095662b1e4477
yunajoe <yunajoe@gmail.com>
2026-08-20 18:06:16 +0900

    문자변경하기

---
+++ src/codetree/codetree101/문자변경하기.java
@@ -0,0 +1,9 @@
+package codetree.codetree101;
+
+public class 문자변경하기 {
+    public static void main(String[] args) {
+        char a = 'C';
+        a =  'T';
+        System.out.println(a);
+    }
+}
