e2b9a51f278266c635e6a368ffc3b5410846255c
yunajoe <yunajoe@gmail.com>
2026-08-20 20:56:55 +0900

    변수값복사하기3

---
+++ src/codetree/codetree101/변수값복사하기3.java
@@ -0,0 +1,17 @@
+package codetree.codetree101;
+
+public class 변수값복사하기3 {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 5;
+        int c = 3;
+        a = c; // 3
+        a = a + c;  // 6
+        b = b - c;  // 2
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
