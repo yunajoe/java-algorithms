f279f9485f060e3364fb3d79c91f92e563772347
yunajoe <yunajoe@gmail.com>
2026-08-24 19:59:40 +0900

    정수선언하고뺄셈

---
+++ src/codetree/novice_low/정수선언하고뺄셈.java
@@ -0,0 +1,10 @@
+package codetree.novice_low;
+
+public class 정수선언하고뺄셈 {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int num1 = 97, num2 = 13;
+        int result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result);
+    }
+}
