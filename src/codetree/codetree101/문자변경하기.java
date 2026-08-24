08453ae7f37a3b72ad99f85f2c5508db35234e43
yunajoe <yunajoe@gmail.com>
2026-08-24 19:12:09 +0900

    단어출력

--- src/codetree/codetree101/문자변경하기.java
+++
@@ -1,9 +0,0 @@
-package codetree.codetree101;
-
-public class 문자변경하기 {
-    public static void main(String[] args) {
-        char a = 'C';
-        a =  'T';
-        System.out.println(a);
-    }
-}
---
+++ src/codetree/novice_low/단어출력.java
@@ -0,0 +1,8 @@
+package codetree.novice_low;
+
+public class 단어출력 {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println("Hello");
+    }
+}
