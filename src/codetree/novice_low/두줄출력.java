4ee5e1159dafdc3f3f407654d3840478ecb6dba1
yunajoe <yunajoe@gmail.com>
2026-08-24 19:35:30 +0900

    두줄출력

---
+++ src/codetree/novice_low/두줄출력.java
@@ -0,0 +1,8 @@
+package codetree.novice_low;
+
+public class 두줄출력 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
