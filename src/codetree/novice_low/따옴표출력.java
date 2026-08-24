6e0126cd509dab9a010a3cdaccaca8a232221b76
yunajoe <yunajoe@gmail.com>
2026-08-24 19:33:27 +0900

    따옴표출력

---
+++ src/codetree/novice_low/따옴표출력.java
@@ -0,0 +1,7 @@
+package codetree.novice_low;
+
+public class 따옴표출력 {
+    public static void main(String[] args) {
+        System.out.println("He says \"It\'s a really simple sentence\".");
+    }
+}
