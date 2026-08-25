9950a78d94825134af31574481eb4d74fc5bf6b0
yunajoe <yunajoe@gmail.com>
2026-08-25 21:24:00 +0900

    변수출력하기

---
+++ src/codetree/novice_low/변수출력하기.java
@@ -0,0 +1,9 @@
+package codetree.novice_low;
+
+public class 변수출력하기 {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+        System.out.printf("%d...%s", a, b);
+    }
+}
