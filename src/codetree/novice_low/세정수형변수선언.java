3b8f6a84e8a4c55db85d7e0a3ecd017333c94920
yunajoe <yunajoe@gmail.com>
2026-08-25 21:22:05 +0900

    세정수형변수선언

---
+++ src/codetree/novice_low/세정수형변수선언.java
@@ -0,0 +1,9 @@
+package codetree.novice_low;
+
+public class 세정수형변수선언 {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7, b = 23, c = 30;
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
