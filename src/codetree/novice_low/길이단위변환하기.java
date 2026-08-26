137ac0a554d782c6a07e55cda050b143dbad4f94
yunajoe <yunajoe@gmail.com>
2026-08-26 19:59:31 +0900

    길이단위변환하기

---
+++ src/codetree/novice_low/길이단위변환하기.java
@@ -0,0 +1,10 @@
+package codetree.novice_low;
+
+public class 길이단위변환하기 {
+    public static void main(String[] args) {
+        double feet = 30.48;
+        double mile = 160934;
+        System.out.printf("%.1fft = %.1fcm\n", 9.2, feet * 9.2);
+        System.out.printf("%.1fmi = %.1fcm\n", 1.3, mile * 1.3);
+    }
+}
