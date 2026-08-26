8ca16200ce1352d39b7978ec9fba0a9a446685c2
yunajoe <yunajoe@gmail.com>
2026-08-26 20:08:22 +0900

    소수점맞춰출력하기

---
+++ src/codetree/novice_low/소수점맞춰출력하기.java
@@ -0,0 +1,9 @@
+package codetree.novice_low;
+
+public class 소수점맞춰출력하기 {
+    public static void main(String[] args) {
+        double a = 5.26, b = 8.27;
+        double result =  a * b;
+        System.out.printf("%.3f", result);
+    }
+}
