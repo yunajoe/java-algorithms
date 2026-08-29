e1b5b8c4fd44738e4d44694513a63cfbb8cec677
yunajoe <yunajoe@gmail.com>
2026-08-29 17:55:47 +0900

    점수입출력

---
+++ src/codetree/novice_low/점수입출력.java
@@ -0,0 +1,13 @@
+package codetree.novice_low;
+
+import java.util.Scanner;
+
+public class 점수입출력 {
+    // Please write your code here.
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        System.out.printf("Your score is %d point.", score);
+    }
+}
