9a0342a362a127d980e24838a0551d35eba12bdf
yunajoe <yunajoe@gmail.com>
2026-08-29 17:59:09 +0900

    점수입력받아계산

---
+++ src/codetree/novice_low/점수입력받아계산.java
@@ -0,0 +1,11 @@
+package codetree.novice_low;
+
+import java.util.Scanner;
+
+public class 점수입력받아계산 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*2+3);
+    }
+}
