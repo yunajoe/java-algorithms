bf5cb2196f7dbb2738d47aeda5499355f01ddd0a
yunajoe <yunajoe@gmail.com>
2026-08-31 21:30:22 +0900

    새실수의반올림

--- src/codetree/novice_low/새실수의반올림.java
+++ src/codetree/novice_low/새실수의반올림.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class 새실수의반올림 {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        double c = sc.nextDouble();
-        System.out.printf("%.3f\n%.3f\n%.3f\n", a, b, c);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a + " " + b + " " + c);
     }
 }
