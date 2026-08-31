ea8ad1cfc4894250b06a106c9b333b9d90608b3b
yunajoe <yunajoe@gmail.com>
2026-08-31 21:28:12 +0900

    새실수의반올림

---
+++ src/codetree/novice_low/새실수의반올림.java
@@ -0,0 +1,13 @@
+package codetree.novice_low;
+
+import java.util.Scanner;
+
+public class 새실수의반올림 {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        System.out.printf("%.3f\n%.3f\n%.3f\n", a, b, c);
+    }
+}
