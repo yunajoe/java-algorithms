8f72245de188a1ea11e8498f31a5c3b0c8c75d8a
yunajoe <yunajoe@gmail.com>
2026-09-06 11:49:21 +0900

    사칙연산

---
+++ src/codetree/novice_low/사칙연산.java
@@ -0,0 +1,17 @@
+package codetree.novice_low;
+
+import java.util.Scanner;
+
+public class 사칙연산 {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
